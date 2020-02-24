74b6a0a0ec7eaa1c2d2bc8a88ab4f63a9f7b91b9
slam3085 <slam3085@gmail.com>
2020-02-24 21:05:55 +0300

    tabs

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main()
 {
-	const int width = 800, height = 400;
+    const int width = 800, height = 400;
     vec3* framebuffer = new vec3[width * height];
-	gradientWithCuda(framebuffer, height, width);
-	std::string filename = "gradient.ppm";
-	render(filename, framebuffer, height, width);
+    gradientWithCuda(framebuffer, height, width);
+    std::string filename = "gradient.ppm";
+    render(filename, framebuffer, height, width);
     delete[] framebuffer;
 }
