829967a04e5d2d1942f3e412847d75479719cc28
slam3085 <slam3085@gmail.com>
2020-03-07 14:08:54 +0300

    fix

--- hittable/transformations.h
+++ hittable/transformations.h
@@ -7,8 +7,8 @@
 struct Translate : public Hittable
 {
     __device__ Translate(Hittable* h, const vec3& o): hittable(h), offset(o) {}
-    __device__ __forceinline__ virtual bool hit(Ray* ray, float t_min, float t_max, HitRecord* rec) const;
-    __device__ __forceinline__ virtual bool bounding_box(float t0, float t1, AABB* box) const;
+    __device__ virtual bool hit(Ray* ray, float t_min, float t_max, HitRecord* rec) const;
+    __device__ virtual bool bounding_box(float t0, float t1, AABB* box) const;
     Hittable* hittable;
     vec3 offset;
 };
